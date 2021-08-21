1701a131d588e787164116670278eaa93b196aea
tatianka144 <tanyshkakoval28@gmail.com>
2021-08-21 18:49:12 +0300

    changes in private fields

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -6,44 +6,44 @@ export default class BackgroundModule extends Module {
     #step
     #color
     #background
+    #canvas
+    #ctx
     constructor(type, text){
         super(type, text);
         this.#gradient;
         this.#step;
-        this.#color;
         this.#background;
 
-        this.canvas = document.createElement('canvas');
-        this.canvas.id = 'canvas';
-        this.canvas.style.height = '100%';
-        this.canvas.style.width = '100%';
-        document.body.append(this.canvas);
-        this.ctx = this.canvas.getContext("2d");
+        this.#canvas = document.createElement('canvas');
+        this.#canvas.id = 'canvas';
+        this.#canvas.style.height = '100%';
+        this.#canvas.style.width = '100%';
+        document.body.append(this.#canvas);
+        this.#ctx = this.#canvas.getContext("2d");
     }
 
-    #randomHsl = () => `hsla(${Math.random() * 360}, 100%, 50%, 1)`
+    #randomHsl = () => `hsla(${Math.random() * 360}, 100%, 50%, 1)`;
 
     #getRandomGradientColor = () => {
         const left = this.#randomHsl(),
             right =  this.#randomHsl(),
             angle = Math.round( Math.random() * 360 );
-        this.#background = [left, right]   
+        this.#background = [left, right];
 
-        this.#gradient = "linear-gradient(" + angle + "deg, " + left + ", " + right + ")"
+        this.#gradient = "linear-gradient(" + angle + "deg, " + left + ", " + right + ")";
     }
 
     #getUrl = async () => {
         try{
             const {url} = await fetch('https://source.unsplash.com/random/1600x900');
-            return await url
+            return await url;
         } catch(e){
-            console.log(e)
+            console.log(e);
         }
     }
 
-    
-    #drawCircles = (item) => {
-        requestAnimationFrame(this.#drawCircles)
+    #drawCircles = () => {
+        requestAnimationFrame(this.#drawCircles);
         let a=0.2,
             t=0,
             aStep = Math.PI * 0.01,
@@ -55,10 +55,10 @@ export default class BackgroundModule extends Module {
             time = performance.now()/360;
         
         a = Math.sin(2-time * 0.0001);
-        t = Math.sin(2+time * this.#step); // can use 0.1 0.003
+        t = Math.sin(2+time * this.#step);
         aStep = Math.PI * (0.375 + Math.sin(time * 0.001) * 0.125);
         
-        this.ctx.fillRect(0,0,canvas.width,canvas.height);
+        this.#ctx.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
 
         for(let theta = 0; theta < totalAngle; theta+=aStep){
             radius = (t + Math.pow(2, Math.cos(theta * a))) * 200;
@@ -68,27 +68,22 @@ export default class BackgroundModule extends Module {
             y = cy + Math.sin(theta) * radius;
             
             if(theta > 0){
-                let hue = ((theta / Math.PI) * 180)*this.#color;
-                this.ctx.beginPath();
-                this.ctx.moveTo(x, y+20);
-                this.ctx.lineTo(px, py+20);
-                let gradient = this.ctx.createLinearGradient(200,0, 220,0);
-                gradient.addColorStop(0, this.#background[0]);
-                gradient.addColorStop(1, this.#background[1]);
+                this.#ctx.beginPath();
+                this.#ctx.moveTo(x, y+20);
+                this.#ctx.lineTo(px, py+20);
 
-                this.ctx.strokeStyle = theta.toFixed()%2 ? this.#background[0] : this.#background[1];
-                this.ctx.lineWidth = .8
-                this.ctx.stroke();
-                this.ctx.closePath();
+                this.#ctx.strokeStyle = theta.toFixed()%2 ? this.#background[0] : this.#background[1];
+                this.#ctx.lineWidth = .8
+                this.#ctx.stroke();
+                this.#ctx.closePath();
             }
         }
     }
 
     #circlesBackground = () => {
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-        this.ctx.fillStyle = this.#background;
-        this.#color = random(1,10)/100;
+        this.#canvas.width = window.innerWidth;
+        this.#canvas.height = window.innerHeight;
+        this.#ctx.fillStyle = this.#background;
         this.#step = Math.random() * (0.1 - 0.003) + 0.003;
         this.#drawCircles();
     }
